3eb1800cd25ed76840202677979c321c6a524b15
Alexis <alexisalvarez1234563@outlook.com>
2024-04-16 14:27:37 -0600

    ahora si ya todo

--- src/app/menu-capturador/menu-capturador.component.ts
+++ src/app/menu-capturador/menu-capturador.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-menu-capturador',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu-capturador.component.css']
 })
 export class MenuCapturadorComponent {
+  constructor(private router: Router, private auth: AuthService){}
 
+  navigateToRegistroCandidatos() {
+    this.router.navigate(['/formulario-solicitud']);
+  }
+
+  navigateToEstatusVisita(){
+    this.router.navigate(['/solicitantes']);
+  }
+
+  logOut(){
+    this.auth.logout();
+    this.router.navigate(['/login']);
+  }
 }
